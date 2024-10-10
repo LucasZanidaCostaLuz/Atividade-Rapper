e0aebc2099b6bc525ebc4df7b84608d00f2f46a7
Lucas Zani <zanin0503@gmail.com>
2024-10-10 09:57:33 -0300

    feat: create method post

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -18,3 +18,33 @@ suspeitoRoutes.get("/", (req, res) => {
   });
 
 export default suspeitoRoutes
+
+suspeitoRoutes.post("/", (req, res) => {
+    const{nome, idade, descricao, envolvimento} = req.body;
+    if (!nome || !Number.isInteger(idade)){
+        return res.status(400).json({
+            message: "coloque um nome valido ou responda uma idade valida seu burro do krl"
+        })
+    }
+
+    if(envolvimento != "sim" && envolvimento != "não"){
+        return res.status(400).json({
+            message: "coloque sim ou não em envolvimento"
+        })
+    }
+
+    const novoSuspeito = {
+        id : Math.floor(Math.random() * 1000000),
+        nome,
+        idade,
+        descricao: descricao || [],
+        envolvimento
+    }
+
+    suspeitos.push(novoSuspeito)
+
+    return res.status(201).json({
+        message: "suspeito denunciado com sucesso", 
+        novoSuspeito
+    })
+})
